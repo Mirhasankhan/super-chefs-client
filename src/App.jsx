423955beea777ff7dbd8c4e78af11b3c3b15323d
Mirhasankhan <mirhasan000034@gmail.com>
2023-05-02 03:47:32 +0600

    project setup done

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,7 @@ import { useEffect } from 'react'
 import './App.css'
 
 function App() {
-
- useEffect(()=>{
-  fetch('http://localhost:5000/chef')
-  .then(res => res.json())
-  .then(data => console.log(data))
- },[])
- useEffect(()=>{
-  fetch('http://localhost:5000/recipes')
-  .then(res => res.json())
-  .then(data => console.log(data))
- },[])
-  return (
-    
+  return (    
     <>
      <h2>how are you</h2>
     </>
---
+++ src/Components/Shared/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            tis is header
+        </div>
+    );
+};
+
+export default Header;
